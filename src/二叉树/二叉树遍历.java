d178d061ed9053ca2a29b78a9ba2e1bb84de739d
Administrator <1522963085@qq.com>
2020-06-07 17:39:48 +0800

    二叉树遍历

--- src/二叉树/二叉树遍历.java
+++ src/二叉树/二叉树遍历.java
@@ -18,14 +18,16 @@ public class 二叉树遍历 {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         TreeNode l = new TreeNode(2);
-        TreeNode r = new TreeNode(4);
-        TreeNode ll = new TreeNode(5);
-        TreeNode lr = new TreeNode(3);
+        TreeNode r = new TreeNode(3);
+        TreeNode lr = new TreeNode(4);
+        TreeNode rl = new TreeNode(5);
+        TreeNode rr = new TreeNode(6);
         root.left = l;
         root.right = r;
-        l.left = ll;
         l.right = lr;
-        List<Integer> result = postorderTraversal2(root);
+        r.left = rl;
+        r.right = rr;
+        List<Integer> result = postorderTraversal(root);
         for (Integer integer : result) {
             System.out.printf(integer.toString()+",");
         }
